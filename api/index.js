af001f95ab37c77a701a1ab895956e75567df0fb
Sarvesh-Shelgaonkar <sarveshshelgaonkar864@gmail.com>
2025-01-21 16:48:32 +0530

    Create User Update API Route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 const app = express();
 dotenv.config();
 mongoose
@@ -11,6 +12,7 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err.message));
 
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
   console.log(`Server is Running on Port 3000!`);
 });
