807e03f702ee732fbceb7df6b9b9f182713b35be
Sarvesh-Shelgaonkar <sarveshshelgaonkar864@gmail.com>
2025-01-26 16:39:52 +0530

    Complete Create Listing Page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRouter from './components/PrivateRouter';
-
+import CreateListing from './pages/CreateListing';
 export default function App() {
   return (
     <BrowserRouter>
@@ -18,6 +18,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route element = {<PrivateRouter/>} >
         <Route path="/profile" element={<Profile />} />
+        <Route path='/create-listing' element={<CreateListing />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import { useRef, useState, useEffect } from "react";
-
+import { Link } from 'react-router-dom';
 import {
   updateUserStart,
   updateUserSuccess,
@@ -10,7 +10,7 @@ import {
   deleteUserSuccess,
   signOutUserStart,
   signOutUserSuccess,
-  signOutUserFailure
+  signOutUserFailure,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 export default function Profile() {
@@ -125,6 +125,9 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
