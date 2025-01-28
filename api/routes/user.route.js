dea993a3f04c8f46001b7a8087a9b2c3c54c3132
Sarvesh-Shelgaonkar <sarveshshelgaonkar864@gmail.com>
2025-01-28 16:04:35 +0530

    Create Get User Listings Api Route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { deleteUser, test } from "../controllers/user.controller.js";
 import {verifyToken} from "../utils/verifyUser.js";
-import { updateUser } from "../controllers/user.controller.js";
+import { updateUser,getUserListings } from "../controllers/user.controller.js";
+import { get } from "mongoose";
 const router = express.Router();
 
 router.get("/test", test);
 router.post('/update/:id',verifyToken, updateUser);
 router.delete('/delete/:id',verifyToken, deleteUser);
+router.get('/listings/:id',verifyToken, getUserListings);
 export default router;
  
