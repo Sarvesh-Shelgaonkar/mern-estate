e12893314fdc1cfe3bfbd8f5b1bbc67f83fcff6f
Sarvesh-Shelgaonkar <sarveshshelgaonkar864@gmail.com>
2025-02-02 18:50:12 +0530

    Create Update Listing Api Route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -18,10 +18,31 @@ export const deleteListing = async (req, res, next) => {
       errorHandler(401, "You are not authorized to delete this listing")
     );
   }
-  try{
+  try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json({ message: "Listing deleted successfully" });
-  }catch(error){
+  } catch (error) {
+    next(error);
+  }
+};
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(
+      errorHandler(401, "You are not authorized to update this listing")
+    );
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
     next(error);
   }
 };
